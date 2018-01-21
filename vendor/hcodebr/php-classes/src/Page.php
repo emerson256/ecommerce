292768ac19d879dea362d7494aa89d3e51f266ba
emerson256 <e.macedohenrique@gmail.com>
2018-01-20 23:36:17 -0200

    Removidos arquivos de configuraçao

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -16,7 +16,7 @@ class Page
 		$this->options = array_merge($this->defaults, $opts);
 		$config = array(
 			"tpl_dir"	=> $_SERVER["DOCUMENT_ROOT"]."/views/",
-			"cache_dir" =>	$_SERVER["DOCUMENT_ROOT"]."/views-cache",
+			"cache_dir" =>	$_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 			"debug"		=> false // set to false to improve the speed 
 		);
 
